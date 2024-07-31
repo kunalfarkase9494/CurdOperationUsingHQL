035d6acd7e9254171a8d138c345af8b66836b2e9
Kunal Farkase <112757045+kunalfarkase9494@users.noreply.github.com>
2024-07-31 23:16:58 +0530

    Update ConnectionFactory.java

--- 8.HQL/src/main/java/com/ConnectionFactory/ConnectionFactory.java
+++ 8.HQL/src/main/java/com/ConnectionFactory/ConnectionFactory.java
@@ -17,13 +17,14 @@ public class ConnectionFactory {
 	public static Session getSession() {
 	
 	Properties p = new Properties();
+	//Database information	
 	p.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
 	p.put(Environment.URL, "jdbc:mysql://localhost:3306/seed");
 	p.put(Environment.USER, "root");
 	p.put(Environment.PASS,"");
 	
-	p.put(Environment.SHOW_SQL, true);
-	p.put(Environment.HBM2DDL_AUTO, "update");
+	p.put(Environment.SHOW_SQL, true);//showing executing query on console 
+	p.put(Environment.HBM2DDL_AUTO, "update"); //create table automatically
 	
 	
 	Configuration cfg = new Configuration();
